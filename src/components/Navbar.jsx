233cae9ff14f98e3ed0f9e83b92ed0674e073663
hp <hp@DESKTOP-F9FBMU7>
2023-07-24 15:17:13 +0300

    change the main page LogIn Button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,46 @@
 import { useState } from "react";
 
-
+import { useNavigate } from "react-router-dom";
 import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 
 
 const Navbar = () => {
   
+  const navigate = useNavigate();
+
   const [active, setActive] = useState("Home");
   const [toggle, setToggle] = useState(false); // this is for the toggle
 
   return (
     <header className="justify-between flex justify-between py-5 uppercase hover:bg-white/10 header"> {/* this all are ultiitys form tailwind classes , navbar is a special navbar org.*/}
-      <img src={logo} alt="hoobank" className=" w-[124px] h-[32px]" />   {/* be carful there is a diffrenet between div and nav, lear how to use it */}
-
+      <img src={logo} alt="hoobank" className=" flex w-[130px] h-[32px]" />   {/* be carful there is a diffrenet between div and nav, lear how to use it */}
+      
       <ul className="list-none sm:flex  justify-end items-center text-dimWhite flex-1"> {/* for the mobile unorederd list <<<i used hidden couse it must be hidden for the mobiles >>>*/}
         {navLinks.map((nav, index) => ( 
           <li 
             key={nav.id}
             className={`font-poppins font-normal cursor-pointer text-[16px]${ 
               active === nav.title ? "text-white" : "text-dimWhite" 
-            } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`} //  for the contacts in the navbar and so on
+            } ${index === navLinks.length - 1 ? "mr-10" : "mr-10"}`} //  for the contacts in the navbar and so on
             onClick={() => setActive(nav.title)}
           >
             <a href={`#${nav.id}`}>{nav.title}</a>
+            
           </li>
+          
         ))}
+      </ul>
+      
+      <ul className="list-none sm:flex justify-end items-center text-dimWhite">
+        <li>
+            <button  onClick={() => navigate('/LogIn')} className="bg-cyan-800 hover:bg-cyan-500 font-bold text-white py-3 px-10 rounded-full">
+                LogIn
+            </button>    
+        </li>
+            
+          
+        
       </ul>
 
       <div className="sm:hidden flex flex-1 justify-end items-center uppercase">
